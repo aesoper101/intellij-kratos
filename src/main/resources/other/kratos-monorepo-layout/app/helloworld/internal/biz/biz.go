b3a36fef788546d213401cf16a9052addad5de30
aesoper <weilanzhuan@163.com>
2022-10-24 12:22:53 +0800

    build: update resources

--- src/main/resources/other/kratos-monorepo-layout/app/helloworld/internal/biz/biz.go
+++ src/main/resources/other/kratos-monorepo-layout/app/helloworld/internal/biz/biz.go
@@ -1,6 +1,14 @@
 package biz
 
-import "github.com/google/wire"
+import (
+	"context"
+	"github.com/aesoper101/kratos-monorepo-layout/app/helloworld/internal/data/ent"
+	"github.com/google/wire"
+)
 
 // ProviderSet is biz providers.
 var ProviderSet = wire.NewSet(NewGreeterLogic)
+
+type Transaction interface {
+	InTx(context.Context, func(ctx *ent.Tx) error) error
+}
